1151c175238ef997c608b3687e9c3e2ce7f3229b
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-08-21 16:07:13 -0300

    add robot_arm model

---
+++ src/meta/robot_arm.jl
@@ -0,0 +1,11 @@
+using ReachabilityAnalysis
+Dict([
+    "opts" => Dict(:T=>10.0, :alg=>TMJets(abs_tol=1e-15, orderT=8, orderQ=1, max_steps=50_000)),
+    "X0"   => Hyperrectangle(low=[1.3, 1.3, -0.2, -0.2], high=[1.7, 1.7, 0.2, 0.2]),
+    "info" => Dict(
+        "name" => "robot_arm",
+        "dim" => 2,
+        "linear" => false,
+        "hybrid" => false
+    )
+])
