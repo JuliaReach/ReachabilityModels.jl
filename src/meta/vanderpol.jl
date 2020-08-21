09d8c47e0ab560e118bf59213f02acdd728b63d0
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-08-21 16:13:39 -0300

    add vanderpol model

---
+++ src/meta/vanderpol.jl
@@ -0,0 +1,11 @@
+using ReachabilityAnalysis
+Dict([
+    "opts" => Dict(:T=>7.0, :alg=>TMJets(),
+    "X0"   => Hyperrectangle(low=[1.25, 2.35], high=[1.55, 2.45]),
+    "info" => Dict(
+        "name" => "vanderpol",
+        "dim" => 2,
+        "linear" => false,
+        "hybrid" => false
+    )
+])
