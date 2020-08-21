2fb0c24a6270e08059ed80e610f89d68937979c0
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-08-21 16:29:55 -0300

    fix lorenz

--- src/models/lorenz.jl
+++ src/models/lorenz.jl
@@ -1,15 +1,14 @@
 module lorenz
 using ReachabilityAnalysis
 @taylorize function lorenz!(dx, x, params, t)
-    σ, β, ρ = params
+    σ, β, ρ = 10.0, 8.0/3.0, 28.0
     dx[1] = σ * (x[2] - x[1])
     dx[2] = x[1] * (ρ - x[3]) - x[2]
     dx[3] = x[1] * x[2] - β * x[3]
     return dx
 end
 
-function model(X0; σ=10.0, β=8.0/3.0, ρ=28.0)
-    params = [σ, β, ρ]
+function model(X0)
     S = @system(x' = lorenz!(x), dim:3)
     return IVP(S, X0)
 end
