cc2f2dc03c66bc52763f033e4162a87cb83e4c84
Marcelo Forets <mforets@gmail.com>
2020-09-07 17:38:39 -0300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,30 @@
 using Documenter, ReachabilityModels
 
-makedocs(;
-    modules=[ReachabilityModels],
-    format=Documenter.HTML(),
-    pages=[
+DocMeta.setdocmeta!(ReachabilityModels, :DocTestSetup, :(using ReachabilityModels); recursive=true)
+
+# Generate notebooks
+#include("generate.jl")
+
+# Generate bibliography
+#include("bibliography.jl")
+
+makedocs(
+    sitename = "Reachability Models",
+    modules = [ReachabilityModels],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    pages = [
         "Home" => "index.md",
+        "Usage" => "usage.md",
+        #"Linear ODEs" => Any["Helicopter" =>  "models/helicopter.md"],
+        #"Nonlinear ODEs" => Any["Lorenz" => "models/lorenz.md"],
+        #"Hybrid ODEs" => Any["Bouncing ball" => "models/bouncing_ball.md"],
+        "References" => "references.md",
+        "About" => "about.md"
     ],
-    repo="https://github.com/mforets/ReachabilityModels.jl/blob/{commit}{path}#L{line}",
-    sitename="ReachabilityModels.jl",
-    authors="Marcelo Forets",
-    assets=String[],
+    strict = false
 )
 
-deploydocs(;
-    repo="github.com/mforets/ReachabilityModels.jl",
+deploydocs(
+    repo = "github.com/JuliaReach/ReachabilityModels.jl",
+    push_preview=true
 )
