67fb0ab3f64f23deb4a741e4d30396c8cfc19bfd
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-09-22 15:16:50 -0300

    fix some models

--- src/models/bouncing_ball_nonlinear/bouncing_ball_nonlinear_meta.jl
+++ src/models/bouncing_ball_nonlinear/bouncing_ball_nonlinear_meta.jl
@@ -1,6 +1,6 @@
 using ReachabilityAnalysis
 Dict([
-    "opts" => Dict(:T=>10.0, :alg=>TMJets(orderT=5, orderQ=2, abs_tol=1e-10)),
+    "opts" => Dict(:T=>10.0, :alg=>TMJets(orderT=5, orderQ=2, abs_tol=1e-10), :disjointness_method=>BoxEnclosure(), :intersect_source_invariant=>false),
     "X0"   => [(2, Hyperrectangle(low=[4.9, -0.2], high=[5.1, 0.0]))],
     "info" => Dict(
         "name" => "bouncing_ball_nonlinear",
