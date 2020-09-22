61f2257ba45d4d6f37b84372363ebc74baaf177d
mforets <mforets@gmail.com>
2020-09-22 15:34:24 -0300

    update bball

--- src/ReachabilityModels.jl
+++ src/ReachabilityModels.jl
@@ -1,5 +1,8 @@
 module ReachabilityModels
 
+using Reexport
+@reexport using ReachabilityAnalysis
+
 include("utils.jl")
 export fetch_model, fetch_meta, list
 export @relpath
--- src/models/bouncing_ball/bouncing_ball_meta.jl
+++ src/models/bouncing_ball/bouncing_ball_meta.jl
@@ -3,7 +3,7 @@ Dict([
     "opts" => Dict(:T=>3.0, :alg=>GLGM06(δ=1e-2, max_order=10),
                    :intersection_method=>TemplateHullIntersection(BoxDirections(2)),
                    :clustering_method=>ZonotopeClustering(),
-                   :intersect_source_invariant=>true),
+                   :intersect_source_invariant=>false),
     "X0"   => Hyperrectangle(low=[10.0, 0.0], high=[10.2, 0.0]),
     "info" => Dict(
         "name" => "bouncing_ball",
