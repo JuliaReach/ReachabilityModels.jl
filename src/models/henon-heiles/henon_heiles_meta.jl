3a2066cb63a250ca4e8d27bbe2dbf2309b305cb5
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-10-21 17:09:38 -0300

    add henon-heiles model

---
+++ src/models/henon-heiles/henon_heiles_meta.jl
@@ -0,0 +1,11 @@
+using ReachabilityAnalysis
+Dict([
+    "opts" => Dict(:T=>100.0, :alg=>TMJets(abs_tol=1e-15)),
+    "X0"   => BallInf([0.1, 0.0, 0.0, 0.5], 0.001),
+    "info" => Dict(
+        "name" => "henon_heiles",
+        "dim" => 4,
+        "linear" => false,
+        "hybrid" => false
+    )
+])
