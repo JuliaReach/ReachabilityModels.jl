4c7bc8827a7b5035825fe4809d3e617f5c65769f
mforets <mforets@gmail.com>
2021-07-25 16:10:56 -0300

    fix comment

--- src/models/helicopter/helicopter.jl
+++ src/models/helicopter/helicopter.jl
@@ -24,7 +24,6 @@ B = read(file, "B")
 X = Universe(28)
 
 # input domain
-#U = Hyperrectangle(zeros(6), fill(0.01, 6))
 U = Hyperrectangle(zeros(6), [0, 0, 0, 0.001, 0.001, 0.001])
 
 function model(X0)
