9f54efd8452571cc8079a74cdcac129297b35b64
schillic <git@christianschilling.net>
2024-03-09 23:43:07 +0100

    revise code

--- src/utils.jl
+++ src/utils.jl
@@ -1,129 +1,89 @@
-RM_dir = joinpath(dirname(pathof(ReachabilityModels)), "..")
+MODEL_DIR = joinpath(dirname(pathof(ReachabilityModels)), "..", "src/models")
 
-function load_model(instance::AbstractString; kwargs...)
-    nakeinstance = replace(instance, Pair(".jl", ""))
-    if haskey(kwargs, :X0)
-        X0 = kwargs[:X0]
-    else
-        X0 = load_meta(nakeinstance)["X0"]
+function load_model(model::String; kwargs...)
+    X0 = get(kwargs, :X0, nothing)
+    if isnothing(X0)
+        X0 = load_meta(model)["X0"]
     end
-    dir = joinpath(RM_dir, "src/models", "$(nakeinstance)/$(nakeinstance).jl")
-    if isfile(dir)
-        include(dir)
-        m = getfield(ReachabilityModels, Symbol(nakeinstance))
-        Base.invokelatest(m.model, X0)
+    file = joinpath(MODEL_DIR, model, model * ".jl")
+    if isfile(file)
+        include(file)
+        model_module = getfield(ReachabilityModels, Symbol(model))
+        Base.invokelatest(model_module.model, X0)
     else
-        throw(ArgumentError("Model $nakeinstance not found"))
+        throw(ArgumentError("Model $model not found"))
     end
 end
 
-function load_meta(instance::AbstractString)
-    nakeinstance = replace(instance, Pair(".jl", ""))
-
-    file = joinpath(RM_dir, "src/models", "$(nakeinstance)/$(nakeinstance)_meta.jl")
+function load_meta(model::String)
+    file = joinpath(MODEL_DIR, model, model * "_meta.jl")
     if isfile(file)
         return include(file)
     else
-        throw(ArgumentError("Meta for $instance not found"))
+        throw(ArgumentError("Meta for $model not found"))
     end
-    return S
 end
 
-function list_models(arg, f)
+function list_models(category, predicate)
     models = Vector()
-    dir = joinpath(RM_dir, "src/models")
-    for model in readdir(dir)
-        meta = include(joinpath(dir, model, model * "_meta.jl"))
-        if f(meta["info"][arg])
-            push!(models, meta["info"]["name"])
+    for model in readdir(MODEL_DIR)
+        meta = include(joinpath(MODEL_DIR, model, model * "_meta.jl"))
+        dict = meta["info"]
+        if predicate(dict[category])
+            push!(models, dict["name"])
         end
     end
     return models
 end
 
-# generate models overview in documentation
+function write_overview_file(models, TYPE, type)
+    open(joinpath(dirname(@__FILE__), "../docs/src/overview_$type.md"), "w") do file
+        print(file,
+              """
+              # $TYPE models
+
+              The following models have $type dynamics.
+
+              | Name  | State dimension |
+              |:------|----------------:|
+              """)
+
+        #   | Name  | State dim | Input dim | Safety Property | Application Domain |
+        #   |:------|----------:|----------:|----------------:|-------------------:|
+        for model in models
+            meta = load_meta(model)["info"]
+            name = replace(meta["name"], "_" => "\\_")
+            println(file, "| **$name** | $(meta["dim"]) |")# $(-) | $(-) | $(-) |")
+        end
+    end
+end
+
+# generate models summary in documentation
 function generate_summary()
+    linear_models = list_models("linear", x -> x)
+    nonlinear_models = list_models("linear", x -> !x)
+    hybrid_models = list_models("hybrid", x -> x)
 
-    ### OVERVIEW
-    open(joinpath(dirname(@__FILE__), "../docs/src/overview.md"), "w") do file
-        nmodels = length(list_models("dim", x -> x > 0))
-        nlinear = length(list_models("linear", x -> x == true))
-        nnonlinear = length(list_models("linear", x -> x == false))
-        nhybrid = length(list_models("hybrid", x -> x == true))
-        return print(file, """
-                           # Models
-                           Here is a table showing the number of models for each type
-                           of system.
+    open(joinpath(dirname(@__FILE__), "../docs/src/overview_total.md"), "w") do file
+        nlinear = length(linear_models)
+        nnonlinear = length(nonlinear_models)
+        nhybrid = length(hybrid_models)
+        ntotal = nlinear + nnonlinear + nhybrid
+        print(file, """
+                    # Model overview
 
-                           | Linear   | Nonlinear   | Hybrid   | Total    |
-                           |:---------|------------:|---------:|---------:|
-                           |$(nlinear)|$(nnonlinear)|$(nhybrid)|$(nmodels)|
-                           """)
+                    The following table shows the number of models for each type of system.
+
+                    | Linear | Nonlinear | Hybrid | Total |
+                    |:------:|:---------:|:------:|:-----:|
+                    |$nlinear|$nnonlinear|$nhybrid|$ntotal|
+                    """)
+        return nothing
     end
 
-    ### LINEAR
-    open(joinpath(dirname(@__FILE__), "../docs/src/models/linear_overview.md"), "w") do file
-        function printrow(model)
-            meta = load_meta(model)["info"]
-            return println(file, "| **$(meta["name"])** | $(meta["dim"]) | $(-) | $(-) | $(-) |")
-        end
+    write_overview_file(linear_models, "Linear", "linear")
 
-        print(file,
-              """
-              # Linear models
-              Here are the models with linear dynamics.
+    write_overview_file(nonlinear_models, "Nonlinear", "nonlinear")
 
-              | Name  | State dim | Input dim | Safety Property | Application Domain | Nominal Runtime (sec) |
-              |:------|----------:|----------:|-----------------:|-------------------:|----------------:|
-              """)
-
-        linear_models = list_models("linear", x -> x == true)
-        for model in linear_models
-            printrow(model)
-        end
-    end
-
-    ### NONLINEAR
-    open(joinpath(dirname(@__FILE__), "../docs/src/models/nonlinear_overview.md"), "w") do file
-        function printrow(model)
-            meta = load_meta(model)["info"]
-            return println(file, "| **$(meta["name"])** | $(meta["dim"]) | $(-) | $(-) | $(-) |")
-        end
-
-        print(file,
-              """
-              # Nonlinear models
-              Here are the models with nonlinear dynamics.
-
-              | Name  | State dim | Input dim | Safesty Property | Application Domain | Nominal Runtime |
-              |:------|----------:|----------:|-----------------:|-------------------:|----------------:|
-              """)
-
-        nonlinear_models = list_models("linear", x -> x == false)
-        for model in nonlinear_models
-            printrow(model)
-        end
-    end
-
-    ### HYBRID
-    open(joinpath(dirname(@__FILE__), "../docs/src/models/hybrid_overview.md"), "w") do file
-        function printrow(model)
-            meta = load_meta(model)["info"]
-            return println(file, "| **$(meta["name"])** | $(meta["dim"]) | $(-) | $(-) | $(-) |")
-        end
-
-        print(file,
-              """
-              # Hybrid models
-              Here are the models with hybrid systems.
-
-              | Name  | State dim | Input dim | Safesty Property | Application Domain | Nominal Runtime |
-              |:------|----------:|----------:|-----------------:|-------------------:|----------------:|
-              """)
-
-        hybrid_models = list_models("hybrid", x -> x == true)
-        for model in hybrid_models
-            printrow(model)
-        end
-    end
+    return write_overview_file(hybrid_models, "Hybrid", "hybrid")
 end
