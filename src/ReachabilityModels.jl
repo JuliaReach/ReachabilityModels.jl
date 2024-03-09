4944033b2092bc8d3644ba9928bc8eb126de5982
schillic <git@christianschilling.net>
2024-03-09 23:45:27 +0100

    remove unused aliases

--- src/ReachabilityModels.jl
+++ src/ReachabilityModels.jl
@@ -6,12 +6,6 @@ using Reexport
 
 include("utils.jl")
 
-# name alias
-fetch_model = load_model
-fetch_meta = load_meta
-
-export fetch_model, fetch_meta,
-       load_model, load_meta,
-       list_models
+export load_model, load_meta, list_models
 
 end  # module
