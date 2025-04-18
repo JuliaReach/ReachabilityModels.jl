78459a3a93c9ef79b3d2b6a4cbbac1b94a1340e2
Documenter.jl <documenter@juliadocs.github.io>
2025-04-18 05:59:47 +0000

    delete history

---
+++ previews/PR139/models/heat.jl
@@ -0,0 +1,24 @@
+module heat
+
+using ReachabilityModels, MAT, SparseArrays, ReachabilityBase.CurrentPath
+
+file = matopen(@current_path("heat", "heat.mat"))
+
+# system matrix
+A = read(file, "A")
+
+# input matrix
+B = sparse([67], [1], [1.0], size(A, 1), 1)
+
+# state domain
+X = Universe(200)
+
+# input domain
+U = BallInf([0.0], 0.5)
+
+function model(X0)
+    S = @system(x' = Ax + Bu, x ∈ X, u ∈ U)
+    return IVP(S, X0)
+end
+
+end
